33a7b1bdc1727689383964d0023584b42b25f82a
fajrin200 <muhammadfajrin412@gmail.com>
2021-02-22 22:05:32 +0700

    Penambahan file

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
-    //
+    public function about (){
+        echo "M Fajrin 1941720010";
+    }
+    
 }
--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function articles ($id){
+        echo "Halaman Artikel dengan ID $id";
+    }
+
 }
